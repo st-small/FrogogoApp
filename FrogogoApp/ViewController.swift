6c0a371d2bb509ed8671c3562ae1c5549a5c19de
Станислав Шияновский <st-small@rambler.ru>
2019-10-30 11:45:38 +0200

    Initial Commit

---
+++ FrogogoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrogogoApp
+//
+//  Created by Станислав Шияновский on 10/30/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
